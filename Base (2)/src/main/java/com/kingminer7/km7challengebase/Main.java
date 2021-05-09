80c2e43c44342d38278f53c960ea37440f8b2295
KM7Studios <83722781+KM7Studios@users.noreply.github.com>
2021-05-09 12:05:03 -0400

    Add files via upload

---
+++ Base (2)/src/main/java/com/kingminer7/km7challengebase/Main.java
@@ -0,0 +1,11 @@
+package com.kingminer7.km7challengebase;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
